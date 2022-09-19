d81ac6c841ba9a4f857afb0e0ed5281c19af66bb
Brad <bycarlson23@gmail.com>
2022-09-19 18:20:37 -0500

    getting routes set up

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Post } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!postData[0]) {
+            res.status(404).json({ message: 'No user with this id!' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No user with this id!' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
